cfb8a2a0903499ec3a1bee96a0802d69799f4aac
Ken Goldfarb <hello@kengoldfarb.com>
2020-03-25 16:27:15 -0400

    config

--- release.config.js
+++ release.config.js
@@ -1,7 +1,14 @@
 const spruceSemanticRelease = require('@sprucelabs/semantic-release')
 
 const config = spruceSemanticRelease({
-	npmPublish: true
+	npmPublish: true,
+	branches: [
+		'master',
+		{ name: 'alpha', prerelease: true },
+		{ name: 'qa', prerelease: true },
+		{ name: 'dev', prerelease: true },
+		{ name: 'prerelease/*', prerelease: true }
+	]
 })
 
 module.exports = config
