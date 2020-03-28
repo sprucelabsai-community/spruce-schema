1d8f6718f215648bcf87ebf74d84df5b017a9d9e
liquidg3 <me@taylorrome.ro>
2020-03-28 16:34:34 -0600

    error fix

--- src/errors/SchemaError.ts
+++ src/errors/SchemaError.ts
@@ -1,7 +1,9 @@
-import { SpruceError } from '@sprucelabs/error'
+import SpruceError from '@sprucelabs/error'
 import { SchemaErrorOptions, SchemaErrorCode } from './types'
 
 export default class SchemaError extends SpruceError<SchemaErrorOptions> {
+
+	/** a readable message */
 	public friendlyMessage(): string {
 		const { options } = this
 
@@ -13,6 +15,10 @@ export default class SchemaError extends SpruceError<SchemaErrorOptions> {
 				message = `${this.message}: :${options.schemaId}${
 					options.additionalDetails ? ` ${options.additionalDetails}` : ''
 				}`
+				break
+			case SchemaErrorCode.InvalidField:
+				breakpoint
+				console.log('what ?')
 				break
 			default:
 				message = this.message
