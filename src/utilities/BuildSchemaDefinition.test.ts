c21c00bfec8e30c2e1585942cfc3758e2ff66823
liquidg3 <me@taylorrome.ro>
2020-04-16 11:26:34 -0600

    build schema mixin fields

---
+++ src/utilities/BuildSchemaDefinition.test.ts
@@ -0,0 +1,101 @@
+import '@sprucelabs/path-resolver/register'
+import BaseTest, { ISpruce, test } from '@sprucelabs/test'
+import { ExecutionContext } from 'ava'
+import { buildSchemaDefinition } from '..'
+import { FieldType } from '../fields/fieldType'
+
+/** Context just for this test */
+interface IContext {}
+
+export default class BuildSchemaDefinitionTest extends BaseTest {
+	@test('Can build schema (will always pass, but fail lint)')
+	protected static async testBasicSchemaBuilding(
+		t: ExecutionContext<IContext>
+	) {
+		const schema = buildSchemaDefinition({
+			id: 'test-1',
+			name: 'test 1',
+			fields: {
+				firstName: {
+					type: FieldType.Text
+				}
+			}
+		})
+		t.assert(schema)
+		t.assert(schema.fields.firstName)
+	}
+
+	@test('Test mixing in field to the schema with object literal')
+	protected static testMixingInFieldsWithObjectLiteral(
+		t: ExecutionContext<IContext>
+	) {
+		const schema = buildSchemaDefinition(
+			{
+				id: 'test-1',
+				name: 'test 1',
+				fields: {
+					firstName: {
+						type: FieldType.Text,
+						label: 'First name'
+					}
+				}
+			},
+			{
+				lastName: {
+					type: FieldType.Text,
+					label: 'Last name'
+				}
+			}
+		)
+
+		t.assert(schema.fields.lastName)
+		t.is(schema.fields.firstName.label, 'First name')
+	}
+
+	@test('Try mixing fields of 2 schemas')
+	protected static testMixingInMultipleSchemas(t: ExecutionContext<IContext>) {
+		const user = buildSchemaDefinition({
+			id: 'test-1',
+			name: 'test 1',
+			fields: {
+				firstName: {
+					type: FieldType.Text,
+					label: 'First name'
+				}
+			}
+		})
+
+		const userWithLastName = buildSchemaDefinition(
+			{
+				id: 'test-1',
+				name: 'test 1',
+				fields: {
+					lastName: {
+						type: FieldType.Text,
+						label: 'Last name'
+					}
+				}
+			},
+			user.fields
+		)
+
+		const userWithToken = buildSchemaDefinition(
+			{
+				id: 'test-1',
+				name: 'test 1',
+				fields: {
+					token: {
+						type: FieldType.Text,
+						label: 'Token'
+					}
+				}
+			},
+			userWithLastName.fields
+		)
+
+		t.is(user.fields.firstName.label, 'First name')
+		t.is(userWithLastName.fields.firstName.label, 'First name')
+		t.is(userWithLastName.fields.lastName.label, 'Last name')
+		t.is(userWithToken.fields.token.label, 'Token')
+	}
+}
--- src/utilities/buildSchemaDefinition.ts
+++ src/utilities/buildSchemaDefinition.ts
@@ -1,8 +1,19 @@
-import { ISchemaDefinition } from '../Schema'
+import { ISchemaDefinition, ISchemaDefinitionFields } from '../Schema'
 
 /** Build a schema type for use in your skill */
-export default function buildSchemaDefinition<T extends ISchemaDefinition>(
-	definition: T
-): T {
-	return definition
+export default function buildSchemaDefinition<
+	T extends ISchemaDefinition,
+	F extends ISchemaDefinitionFields
+>(definition: T, mixinFields?: F): T & { fields: F } {
+	const copied = { ...definition }
+	if (mixinFields) {
+		if (!copied.fields) {
+			copied.fields = {}
+		}
+		copied.fields = {
+			...copied.fields,
+			...mixinFields
+		}
+	}
+	return copied as T & { fields: F }
 }
