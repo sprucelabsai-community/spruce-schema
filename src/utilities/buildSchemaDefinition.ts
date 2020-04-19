56d0dc6b896c327fa0932ce7631fa29230b2436c
liquidg3 <me@taylorrome.ro>
2020-04-19 11:25:06 -0600

    fix: comment spelling fixes

--- src/utilities/buildSchemaDefinition.ts
+++ src/utilities/buildSchemaDefinition.ts
@@ -23,7 +23,7 @@ export default function buildSchemaDefinition<
 	return t
 }
 
-// TODO proposal, schema field inherence, second arg is fields mixed into schema and maintain narrow typing
+// TODO proposal, schema field inheritance, second arg is fields mixed into schema and maintain narrow typing
 // Export default function buildSchemaDefinition<
 // 	T extends ISchemaDefinition,
 // 	F extends { [name: string]: FieldDefinition }
