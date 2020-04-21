9b40c3730d685c6d40f5fa9e1e872224bc283af6
liquidg3 <me@taylorrome.ro>
2020-04-21 06:55:15 -0600

    fix: non-narrowing buildSchemaDefinition for better typeing in input

--- src/utilities/buildSchemaDefinition.ts
+++ src/utilities/buildSchemaDefinition.ts
@@ -1,42 +1,8 @@
 import { ISchemaDefinition } from '../Schema'
-// Import { FieldDefinition } from '#spruce:schema/fields/fields.types'
-// TODO figure out how to get field mixing to type properly with SchemaDefinitionValues
 
-type Narrowable =
-	| string
-	| number
-	| boolean
-	| symbol
-	| object
-	| null
-	| undefined
-	| void
-	| ((...args: any[]) => any)
-	| {}
-
-/** Define a schema type from a value with no widening */
-export default function buildSchemaDefinition<
-	S extends ISchemaDefinition,
-	T extends { [K in keyof S]: V | T },
-	V extends Narrowable
->(t: T): T {
-	return t
+/** Builds a schema definition */
+export default function buildSchemaDefinition<T extends ISchemaDefinition>(
+	definition: T
+): T {
+	return definition
 }
-
-// TODO proposal, schema field inheritance, second arg is fields mixed into schema and maintain narrow typing
-// Export default function buildSchemaDefinition<
-// 	T extends ISchemaDefinition,
-// 	F extends { [name: string]: FieldDefinition }
-// >(definition: T, mixinFields?: F): T & { fields: F } {
-// 	const copied = { ...definition }
-// 	if (mixinFields) {
-// 		if (!copied.fields) {
-// 			copied.fields = {}
-// 		}
-// 		copied.fields = {
-// 			...copied.fields,
-// 			...mixinFields
-// 		}
-// 	}
-// 	return copied as T & { fields: F }
-// }
