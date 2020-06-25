c63a9c5e7cbaca76b8d073ae2073c1534dc68d2d
liquidg3 <me@taylorrome.ro>
2020-06-25 15:35:31 -0600

    patch: tmp fix to include fieldclassmap later in the factory code

--- src/factories/FieldFactory.ts
+++ src/factories/FieldFactory.ts
@@ -1,13 +1,12 @@
-import { FieldClassMap } from '#spruce:schema/fields/fieldClassMap'
 import { FieldDefinition, IFieldMap } from '#spruce:schema/fields/fields.types'
 // let firstRun = true
+let FieldClassMap: Record<string, any>
 
 export default class FieldFactory {
 	/** Factory for creating a new field from a definition */
 	public static field<F extends FieldDefinition>(
 		name: string,
-		definition: F,
-		fieldClassMap: typeof FieldClassMap = FieldClassMap
+		definition: F
 	): IFieldMap[F['type']] {
 		// this will pull in augmentations to the field class map object, only needed first load
 		// if (firstRun) {
@@ -16,13 +15,19 @@ export default class FieldFactory {
 		// 	firstRun = false
 		// }
 
-		const fieldClass = fieldClassMap[definition.type]
+		if (!FieldClassMap) {
+			FieldClassMap = require('#spruce:schema/fields/fieldClassMap')
+				.FieldClassMap
+		}
+
+		const fieldClass = FieldClassMap[definition.type]
 
 		if (!fieldClass) {
 			throw new Error(
 				`Failed to find schema field by type "${definition.type}" for field named ${name}.`
 			)
 		}
+
 		// @ts-ignore
 		const field = new fieldClass(name, definition)
 		return field
