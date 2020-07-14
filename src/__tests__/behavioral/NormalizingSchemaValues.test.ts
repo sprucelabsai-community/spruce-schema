b18b4de8c06bdd175008b8a170f5b9364132769b
liquidg3 <me@taylorrome.ro>
2020-07-14 12:01:29 -0600

    patch: finish

---
+++ src/__tests__/behavioral/NormalizingSchemaValues.test.ts
@@ -0,0 +1,33 @@
+import { test, assert } from '@sprucelabs/test'
+import FieldType from '#spruce/schemas/fields/fieldTypeEnum'
+import AbstractSchemaTest from '../../AbstractSchemaTest'
+import buildSchemaDefinition from '../../utilities/buildSchemaDefinition'
+import normalizeSchemaValues from '../../utilities/normalizeSchemaValues'
+
+export default class NormalizingSchemaValues extends AbstractSchemaTest {
+	private static personDefinition = buildSchemaDefinition({
+		id: 'testPerson',
+		name: 'A test person',
+		fields: {
+			firstName: {
+				type: FieldType.Text,
+				isRequired: true,
+			},
+			age: {
+				type: FieldType.Number,
+			},
+		},
+	})
+
+	@test()
+	protected static normalizesSimpleAsExpected() {
+		const values = normalizeSchemaValues(this.personDefinition, {
+			// @ts-ignore
+			firstName: 12345,
+			// @ts-ignore
+			age: '10',
+		})
+
+		assert.isEqualDeep(values, { firstName: '12345', age: 10 })
+	}
+}
