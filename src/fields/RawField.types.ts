4ff3f718669aa1ce8dc4f31e29de4d260c7d2b90
liquidg3 <me@taylorrome.ro>
2020-09-25 09:25:19 -0600

    minor: name optional

--- src/fields/RawField.types.ts
+++ src/fields/RawField.types.ts
@@ -1,7 +1,6 @@
 import { IFieldDefinition } from './field.static.types'
 
 export type IRawFieldDefinition = IFieldDefinition<any> & {
-	/** * .Raw - Deprecated, don't use */
 	type: 'raw'
 	options: {
 		valueType: string
