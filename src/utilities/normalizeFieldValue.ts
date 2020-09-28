ffa04bc57358f217bbf6b8ca452885e54199d04b
liquidg3 <me@taylorrome.ro>
2020-09-28 07:55:37 -0600

    lint

--- src/utilities/normalizeFieldValue.ts
+++ src/utilities/normalizeFieldValue.ts
@@ -14,7 +14,7 @@ export default function normalizeFieldValue<
 	CreateEntityInstances extends boolean
 >(
 	schemaId: string,
-	schemaName?: string,
+	schemaName: string | undefined,
 	schemasById: ISchemasById,
 	field: F,
 	value: any,
@@ -47,6 +47,7 @@ export default function normalizeFieldValue<
 			throw new SpruceError({
 				code: 'INVALID_FIELD',
 				schemaId,
+				schemaName,
 				errors: [
 					{
 						name: field.name,
