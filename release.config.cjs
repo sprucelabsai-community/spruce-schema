7573f201e11fb7476bb5aff5f67c026f79502e8c
Ryan Johnson <ryan@sprucelabs.ai>
2021-02-19 15:32:54 -0500

    patch:  semantic release can find its cjs config

--- release.config.cjs
+++ release.config.cjs
@@ -1,7 +1,7 @@
 const spruceSemanticRelease = require('@sprucelabs/semantic-release')
 
 const config = spruceSemanticRelease.default({
-	config: spruceSemanticRelease.ReleaseConfiguration.Package
+	config: spruceSemanticRelease.ReleaseConfiguration.Package,
 })
 
 module.exports = config
