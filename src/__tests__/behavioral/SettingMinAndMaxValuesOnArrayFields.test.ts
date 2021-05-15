cba840c8b197477c162a374e8255beca0fcf4efd
CircleCI <devops@sprucelabs.ai>
2021-05-15 16:57:32 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/__tests__/behavioral/SettingMinAndMaxValuesOnArrayFields.test.ts
+++ src/__tests__/behavioral/SettingMinAndMaxValuesOnArrayFields.test.ts
@@ -1,5 +1,5 @@
-import { errorAssertUtil } from '@sprucelabs/test-utils'
 import { test, assert } from '@sprucelabs/test'
+import { errorAssertUtil } from '@sprucelabs/test-utils'
 import AbstractSchemaTest from '../../AbstractSchemaTest'
 import buildSchema from '../../utilities/buildSchema'
 import validateSchemaValues from '../../utilities/validateSchemaValues'
