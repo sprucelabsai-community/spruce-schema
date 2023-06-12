3fe17b0d49959b8d7befa20fd1086c817f749707
Taylor Romero <me@taylorrome.ro>
2023-06-12 14:57:26 -0600

    patch: number tests

--- src/__tests__/behavioral/PhoneNumberFormatter.test.ts
+++ src/__tests__/behavioral/PhoneNumberFormatter.test.ts
@@ -1,6 +1,8 @@
 import { test, assert } from '@sprucelabs/test-utils'
 import AbstractSchemaTest from '../../AbstractSchemaTest'
-import { isValidNumber } from '../../utilities/formatPhoneNumber'
+import formatPhoneNumber, {
+	isValidNumber,
+} from '../../utilities/formatPhoneNumber'
 
 export default class PhoneNumberFormatterTest extends AbstractSchemaTest {
 	@test()
@@ -9,6 +11,22 @@ export default class PhoneNumberFormatterTest extends AbstractSchemaTest {
 		this.assertValidNumber('555 555 5555')
 		this.assertValidNumber('+1 (555) 555 5555')
 		this.assertValidNumber('1 555 555 5555')
+	}
+
+	@test()
+	protected static async formatsNumberInDifferentFormats() {
+		this.assertFormatsAsExpected('555 555 5555', '+1 555-555-5555')
+		this.assertFormatsAsExpected('5555555555', '+1 555-555-5555')
+		this.assertFormatsAsExpected('+1 (555) 555-5555', '+1 555-555-5555')
+		this.assertFormatsAsExpected('+1 555 555-5555', '+1 555-555-5555')
+		this.assertFormatsAsExpected('+1 (555)-555-5555', '+1 555-555-5555')
+		this.assertFormatsAsExpected('+15555555555', '+1 555-555-5555')
+		this.assertFormatsAsExpected('+1 (978) 505-2323', '+1 978-505-2323')
+	}
+
+	private static assertFormatsAsExpected(number: string, expected: string) {
+		const actual = formatPhoneNumber(number)
+		assert.isEqual(actual, expected)
 	}
 
 	private static assertValidNumber(number: string) {
